f5aaf0956e222dbd9814ed5656a8de13b41750b9
Jackarias <javier.arias88@gmail.com>
2025-04-14 17:33:43 -0300

    Added route feature to sample react app

--- labs/front_react_dynamic_env/src/App.js
+++ labs/front_react_dynamic_env/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './style.css';
 import * as Constants from './constants.js';
 
 export default function App() {
   return (
-    <div>
-      <h1>Sample website! "{Constants.RANDOM_VAR}"</h1>
+    <div className="App">
+
+      <h1>Sample React Website</h1>
+
+      <Routes>
+        <Route
+          path="/"
+          element={<div>This is root path! "{Constants.RANDOM_VAR}"</div>}
+        />
+
+        <Route
+          path="/sample"
+          element={<div>This is a sample path! "{Constants.RANDOM_VAR}"</div>}
+        />
+
+        <Route
+          path="*"
+          element={<div>404 Page Not Found</div>}
+        />
+      </Routes>
+
     </div>
   );
 }
--- labs/front_react_dynamic_env/src/index.js
+++ labs/front_react_dynamic_env/src/index.js
@@ -1,12 +1,14 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom'; // <-- Import BrowserRouter
 import App from './App';
 
 const root = createRoot(document.getElementById('app'));
 
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
